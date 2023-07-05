41bd6468eb31962c7ec219f1de3c9ec51726ec73
Kenney <mail@kenney.nl>
2023-07-05 11:54:14 +0200

    Updated Seeds Mod
    - New placement sound
    - Trees spawn with random height
    - Only play sound when placement was succesful
    - Added particle effects
    - Trees don't overwrite existing blocks
    - Trees stop growing when hitting an obstacle

--- seedsmod/mod.js
+++ seedsmod/mod.js
@@ -4,9 +4,11 @@
         id = "seeds";
         name = "Seeds Mod";
         chatName = "Seeds";
-        version = 2;
+        version = 3;
 
         onGameStart = (game) => {
+			
+			game.addImage("particles/leaves", game.getModResource("particle-leaves.png"));
 			
 			game.addImage("tools/seeds-tree", game.getModResource("tool-seeds-tree.png"));
 			game.addTool("tools/seeds-tree", 0, "seeds-tree", true);
@@ -17,33 +19,68 @@
 			game.addImage("tools/seeds-flower", game.getModResource("tool-seeds-flower.png"));
 			game.addTool("tools/seeds-flower", 0, "seeds-flower", true);
 			
+			game.addAudio("place-foliage", game.getModResource("place-foliage.mp3"));
+			
         };
 
         onUseTool = (game, player, x, y, layer, toolId) => {
 			
 			if (toolId === "seeds-tree"){
 				
-				// 16: tree trunk bottom
-				// 17: tree thunk middle
-				// 5 : tree leaves
+				if(game.getBlock(x, y, layer) === 0){
 				
-				game.setBlock(x, y, layer, 16);
-				
-				for(let i = 1; i < 3; i++) {
+					// 16: tree trunk bottom
+					// 17: tree thunk middle
+					// 5 : tree leaves
 					
-					game.setBlock(x, y - i, layer, 17);
+					var tree_height = 2 + Math.floor(Math.random() * 3);
+					
+					game.setBlock(x, y, layer, 16);
+					
+					for(let i = 1; i < tree_height; i++){
+						
+						if(game.getBlock(x, y - i, layer) === 0){
+						
+							game.setBlock(x, y - i, layer, 17);
+							
+						}else{
+							
+							tree_height = i;
+							
+							break;
+							
+						}
+						
+					}
+					
+					for(let i = 0; i < 3; i++) {
+						
+						if(game.getBlock(x - 1, y - tree_height - i, layer) === 0){
+							
+							game.setBlock(x - 1, y - tree_height - i, layer, 5);
+							game.addParticlesAtTile("particles/leaves", x - 1, y - tree_height - i, 4);
+							
+						}
+						
+						if(game.getBlock(x + 0, y - tree_height - i, layer) === 0){
+							
+							game.setBlock(x + 0, y - tree_height - i, layer, 5);
+							game.addParticlesAtTile("particles/leaves", x + 0, y - tree_height - i, 4);
+							
+						}
+						
+						if(game.getBlock(x + 1, y - tree_height - i, layer) === 0){
+							
+							game.setBlock(x + 1, y - tree_height - i, layer, 5);
+							game.addParticlesAtTile("particles/leaves", x + 1, y - tree_height - i, 4);
+							
+						}
+						
+					}
+					
+					game.playSfx("place-foliage");
 					
 				}
-				
-				for(let i = 0; i < 3; i++) {
-					
-					game.setBlock(x - 1, y - 3 - i, layer, 5);
-					game.setBlock(x, y - 3 - i, layer, 5);
-					game.setBlock(x + 1, y - 3 - i, layer, 5);
-					
-				}
-				
-				game.playSfx("jump");
 				
 			}
 			
@@ -54,12 +91,13 @@
 					if(game.getBlock(x, y, layer) === 0){
 						
 						game.setBlock(x, y, layer, 9 + Math.floor(Math.random() * 4));
+						game.addParticlesAtTile("particles/leaves", x, y, 4);
+						
+						game.playSfx("place-foliage");
 						
 					}
 					
 				}
-				
-				game.playSfx("jump");
 				
 			}
 			
@@ -70,12 +108,13 @@
 					if(game.getBlock(x, y, layer) === 0){
 						
 						game.setBlock(x, y, layer, 13 + Math.floor(Math.random() * 3));
+						game.addParticlesAtTile("particles/leaves", x, y, 4);
+						
+						game.playSfx("place-foliage");
 						
 					}
 					
 				}
-				
-				game.playSfx("jump");
 				
 			}
 			
